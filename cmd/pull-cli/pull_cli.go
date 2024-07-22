a787ebd1052a8f8c36502cfa1b3d708b715e4692
Jonas Peterburs <j.peterburs@protonmail.com>
2024-07-22 18:44:48 +0000

    Implement github package into cmd

--- cmd/pull-cli/pull_cli.go
+++ cmd/pull-cli/pull_cli.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/jpeterburs/pull-cli/internal/github"
+	"github.com/google/go-github/v63/github"
 )
 
 func main() {
-	pr := github.PullRequest{
-		Number: 1,
-		Repository: &github.Repository{
-			Owner: "jpeterburs",
-			Name:  "pull-cli",
-		},
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	if token == "" {
+		log.Fatal("Unauthorized: No token present")
 	}
-	fmt.Println(pr.String())
+
+	ctx := context.Background()
+	client := github.NewClientWithEnvProxy().WithAuthToken(token)
+
+	pr, _, err := client.PullRequests.Get(ctx, "jpeterburs", "pull-cli", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(pr.GetTitle())
 }
